4221ee6c99c3a4105db85572baa50d3d3329c8ec
patacca <patacca@autistici.org>
2026-04-04 12:00:15 +0000

    fix(native): restore nanobind STL casters

--- src/flatline/native/session.h
+++ src/flatline/native/session.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <nanobind/nanobind.h>
+#include <nanobind/stl/pair.h>
+#include <nanobind/stl/string.h>
+#include <nanobind/stl/vector.h>
 
 #include <string>
 #include <utility>
--- src/flatline/native/shared.h
+++ src/flatline/native/shared.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <nanobind/nanobind.h>
+#include <nanobind/stl/string.h>
 
 #include <cstddef>
 #include <cstdint>
